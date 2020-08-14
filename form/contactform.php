a85c2334f11a6d7941cf2e1277707c4648f7a0c9
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-13 22:55:00 -0700

    Working on form

--- form/contactform.php
+++ form/contactform.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "stephanieraymos@gmail.com"
+    $mailTo = "raymosstephanie@outlook.com"
     $headers = "From: ".$mailFrom;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
