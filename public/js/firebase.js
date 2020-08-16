96f64c9805d64c03814be0abbd130e03f62afb8d
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-15 18:49:05 -0700

    Config edit

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -9,40 +9,42 @@ const inputEmail = form.querySelector('#inputEmail');
 
 //config your firebase push
 const config = {
-    apiKey: "AIzaSyCfI_1234455glTdTNjFA3kAvmimlU",
-    authDomain: "example-9167e.firebaseapp.com",
-    databaseURL: "example-9167e.firebaseio.com",
-    projectId: "example-9167e",
-    storageBucket: "example-9167e.appspot.com",
-    messagingSenderId: "6271234586832"
+    apiKey: "AIzaSyAGyrO7a2kKLOXA75pDumpdlZ9rachXOOY",
+    authDomain: "portfolio-df0a4.firebaseapp.com",
+    databaseURL: "https://portfolio-df0a4.firebaseio.com",
+    projectId: "portfolio-df0a4",
+    storageBucket: "portfolio-df0a4.appspot.com",
+    messagingSenderId: "992167096233",
+    appId: "1:992167096233:web:9f7320eae316447a6e5eb8",
+    measurementId: "G-P1W8F07ZM7"
 };
 
 
 //create a functions to push
-    function firebasePush(input) {
+function firebasePush(input) {
 
 
-        //prevents from braking
-        if (!firebase.apps.length) {
-            firebase.initializeApp(config);
-        }
-
-        //push itself
-        var mailsRef = firebase.database().ref('emails').push().set(
-            {
-                mail: input.value
-            }
-        );
-
+    //prevents from braking
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
     }
+
+    //push itself
+    var mailsRef = firebase.database().ref('emails').push().set(
+        {
+            mail: input.value
+        }
+    );
+
+}
 
 //push on form submit
-    if (form) {
-        form.addEventListener('submit', function (evt) {
-            evt.preventDefault();
-            firebasePush(inputEmail);
+if (form) {
+    form.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        firebasePush(inputEmail);
 
-            //shows alert if everything went well.
-            return alert('Data Successfully Sent to Realtime Database');
-        })
-    }
+        //shows alert if everything went well.
+        return alert('Data Successfully Sent to Realtime Database');
+    })
+}
